4c1be436337b58f8180f25dc6149b06834185e03
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2018-02-25 20:38:32 -0300

    Feito no VS Code

---
+++ src/ExercicioDeControleDeFluxo.java
@@ -0,0 +1,20 @@
+/* Criar uma Variavel salario e imprimir seu imposto
+Tabela de imposto
+
+salario < 1000 5%
+salario >= 1000 && salario < 2000 10%
+salario >= 2000 && salario < 4000 15%
+salario > 5000 20%
+
+*/
+
+/**
+ * ExercicioDeControleDeFluxo
+ */
+public class ExercicioDeControleDeFluxo {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!!");
+    }
+        
+    }
+    
