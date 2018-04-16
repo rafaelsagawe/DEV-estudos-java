b7f5c4dd086f259b177fa4e96de4030fa5e3a736
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2018-04-16 20:02:29 -0300

    Loop com contador

--- src/Loop.java
+++ src/Loop.java
@@ -2,9 +2,9 @@ public class Loop{
   public static void main(String[] args) {
     int x = 0;
 
-    while (x<=9) {
+    while (x<=98) {
       x = x + 1;
-      System.out.println("Olá, sou seu primeiro looping");
+      System.out.println(x + " - Olá, sou seu primeiro looping");
     }
   }
 
