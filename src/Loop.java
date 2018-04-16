ba170be81b91a7aefd43a703cc582c504016c630
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2018-04-16 19:42:05 -0300

    Loop

---
+++ src/Loop.java
@@ -0,0 +1,11 @@
+public class Loop{
+  public static void main(String[] args) {
+    int x = 0;
+
+    while (x<=9) {
+      x = x + 1;
+      System.out.println("Olá, sou seu primeiro looping");
+    }
+  }
+
+}
