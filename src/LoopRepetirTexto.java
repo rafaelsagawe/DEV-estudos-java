1c54379021d4f78ac02f9eb76f4499e2d2570f37
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2018-04-21 21:58:20 -0300

    Teste de Loop

---
+++ src/LoopRepetirTexto.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LoopRepetirTexto{
+  public static void main(String[] args) {
+    int x = 0;
+
+    Scanner in = new Scanner(System.in);
+    String texto;
+    //int beta;
+
+    System.out.println("Texto que será repetido: ");
+    texto = in.String();
+    //beta = in.nextInt();
+    while (x<=9) {
+      x = x + 1;
+      System.out.println(x + texto);
+    }
+  }
+
+}
