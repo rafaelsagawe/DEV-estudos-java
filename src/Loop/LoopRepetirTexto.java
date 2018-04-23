ec611659d4bef0fbe6142e91e53bc675e7074a08
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2018-04-23 10:37:42 -0300

    Ecolher terto que ira repetir

--- src/Loop/LoopRepetirTexto.java
+++ src/Loop/LoopRepetirTexto.java
@@ -4,16 +4,17 @@ public class LoopRepetirTexto{
   public static void main(String[] args) {
     int x = 0;
 
+    // Entrada de texto
     Scanner in = new Scanner(System.in);
-    String texto;
-    //int beta;
-
+    String txt;
     System.out.println("Texto que será repetido: ");
-    texto = in.String();
-    //beta = in.nextInt();
+    txt = in.next();
+    // Final da Entrada de texto
+
+    // Repetir o texto escolhido 10 vezes.
     while (x<=9) {
       x = x + 1;
-      System.out.println(x + texto);
+      System.out.println(x + " - " + txt);
     }
   }
 }
