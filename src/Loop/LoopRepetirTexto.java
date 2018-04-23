d8606ad81b6dbd0e159f4ff79c39a18464890764
Rafael Sagawe <sagawe_r@hotmail.com>
2018-04-23 00:28:32 -0300

    LoopRepetirTexto funcionando repetindo 6 vezes

--- src/Loop/LoopRepetirTexto.java
+++ src/Loop/LoopRepetirTexto.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class LoopRepetirTexto{
   public static void main(String[] args) {
     int x = 0;
 
-    while (x<=4) {
-      x = x + 1;
-      System.out.println("Olá");
+    Scanner in = new Scanner(System.in);
+    String texto;
+    //int beta;
 
+    System.out.println("Texto que será repetido: ");
+    texto = in.String();
+    //beta = in.nextInt();
+    while (x<=9) {
+      x = x + 1;
+      System.out.println(x + texto);
     }
   }
 }
