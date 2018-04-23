3d73cc67628aa78e55cdac24bf12e6864ca3b4e8
Rafael Sagawe <sagawe_r@hotmail.com>
2018-04-22 23:54:46 -0300

    O usuario deve digitar um texto e o terminal deve repetir o texto

---
+++ src/Loop/LoopRepetirTexto.java
@@ -0,0 +1,11 @@
+public class LoopRepetirTexto{
+  public static void main(String[] args) {
+    int x = 0;
+
+    while (x<=4) {
+      x = x + 1;
+      System.out.println("Olá");
+
+    }
+  }
+}
--- src/LoopRepetirTexto.java
+++ src/LoopRepetirTexto.java
@@ -16,5 +16,4 @@ public class LoopRepetirTexto{
       System.out.println(x + texto);
     }
   }
-
 }
